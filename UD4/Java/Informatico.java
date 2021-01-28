f8a672563d25a09e1a3af555b7918f88ceb70c7e
Javier Baena Santa-Cruz <73710320+jb-coder@users.noreply.github.com>
2021-01-28 10:11:41 +0100

    Rename UD4/Informatico.java to UD4/Java/Informatico.java

--- UD4/Java/Informatico.java
+++ UD4/Java/Informatico.java
@@ -3,12 +3,12 @@ package empleados;
 public class Informatico extends Empleado { //Clase hija de la clase padre Empleado
 	//Atributos	
 	private int numeroSoportes;
-	//Método constructor	
+	//MÃ©todo constructor	
 	public Informatico(String nombre, String direccion, int telefono, String fecha_nac, double sueldo,int numeroSoportes) {
 		super(nombre, direccion, telefono, fecha_nac, sueldo); //Herencia de atributos de la clase padre
 		this.numeroSoportes = numeroSoportes;
 	}
-	//Métodos getter y setter para obtener y asignar los valores de los atributos
+	//MÃ©todos getter y setter para obtener y asignar los valores de los atributos
 	public int getNumeroSoportes() {
 		return numeroSoportes;
 	}
@@ -17,7 +17,7 @@ public class Informatico extends Empleado { //Clase hija de la clase padre Emple
 		this.numeroSoportes = numeroSoportes;
 	}
 	
-	//Método para obtener toda la información por consola de la clase Empleado e Informatico		
+	//MÃ©todo para obtener toda la informaciÃ³n por consola de la clase Empleado e Informatico		
 	public void getDatosinf() {
 		getDatosemplado();
 		System.out.println( "Numero de soportes: " +getNumeroSoportes());

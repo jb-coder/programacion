64c978bc7b65212820fd5d177d3ff789821a3406
Javier Baena Santa-Cruz <73710320+jb-coder@users.noreply.github.com>
2021-01-28 10:10:55 +0100

    Rename UD4/Contable.java to UD4/Java/Contable.java

--- UD4/Java/Contable.java
+++ UD4/Java/Contable.java
@@ -4,14 +4,14 @@ public class Contable extends Empleado { //Clase hija de la clase padre Empleado
 	//Atributos	
 	private int numeroingresos;
 	private int numeroegresos;
-	//Método constructor	
+	//MÃ©todo constructor	
 	public Contable(String nombre, String direccion, int telefono, String fecha_nac, double sueldo, int numeroingresos,
 			int numeroegresos) {
 		super(nombre, direccion, telefono, fecha_nac, sueldo); //Herencia de atributos de la clase padre
 		this.numeroingresos = numeroingresos;
 		this.numeroegresos = numeroegresos;
 	}
-	//Métodos getter y setter para obtener y asignar los valores de los atributos
+	//MÃ©todos getter y setter para obtener y asignar los valores de los atributos
 	public int getNumeroingresos() {
 		return numeroingresos;
 	}
@@ -27,7 +27,7 @@ public class Contable extends Empleado { //Clase hija de la clase padre Empleado
 	public void setNumeroegresos(int numeroegresos) {
 		this.numeroegresos = numeroegresos;
 	}
-	//Método para obtener toda la información por consola de la clase Empleado y contable	
+	//MÃ©todo para obtener toda la informaciÃ³n por consola de la clase Empleado y contable	
 	public void getDatoscont() {
 		getDatosemplado();
 		System.out.println( "Numero de ingresos: " +getNumeroingresos()+

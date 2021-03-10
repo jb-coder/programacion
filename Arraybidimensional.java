4328a75448012848d26dad9c7e30976242239bb9
Javier Baena Santa-Cruz <73710320+jb-coder@users.noreply.github.com>
2021-03-10 08:15:07 +0100

    Add files via upload

---
+++ Arraybidimensional.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Arraybidimensional {
+
+	public static void main(String[] args) {
+		
+		//Objeto Scanner
+		try(Scanner in=new Scanner(System.in)){
+		
+			//Se solicita al usuario la longitud de las filas del array
+			System.out.println("Cuantas filas desea que tenga el array?");
+			
+			//Almacenamos el dato en la variable filas
+			int filas=in.nextInt();
+			
+			//Se solicita al usuario la longitud de las columnas del array
+			System.out.println("Cuantas columnas desea que tenga el array?");
+					
+			//Almacenamos el dato en la variable filas
+			int columnas=in.nextInt();
+			
+			//Creamos el array bidimensional con la longitud introducida
+			//por el usuario
+			int array[][]=new int[filas][columnas];
+			
+			//Creamos un bucle For anidado para rellenar de numeros enteros aleatorios
+			//del 1 al 10 con la funcion numeroAleatorio el array
+			
+			for(int i=0;i<filas;i++) {
+				
+				for(int j=0;j<columnas;j++) {
+					
+					array[i][j]=numeroAleatorio(1,10);
+					System.out.println("Fila "+i+" Columna "+j+" Contenido: "+array[i][j]);
+				}
+				
+			}
+			System.out.println("El contenido completo del array es: "+array[filas][columnas]);
+			
+			} catch (Exception e) {			
+		}
+	}
+	
+	//Función numero aleatorio entre dos numeros enteros que le demos
+    public static int numeroAleatorio(int minimo, int maximo){
+        return (int)Math.floor(Math.random()*(minimo-(maximo+1))+(maximo+1));
+    }
+
+}
---
+++ Arrayunidimensional.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Arrayunidimensional {
+
+	public static void main(String[] args) {
+		
+		//Objeto Scanner
+		try(Scanner in=new Scanner(System.in)){
+		
+			//Se solicita al usuario la longitud del array
+			System.out.println("Que longitud desea que tenga el array?");
+		
+			//Almacenamos el dato en la variable longitud
+			int longitud=in.nextInt();
+
+			//Creamos el array unidimensional con la longitud introducida
+			//por el usuario
+			int array[]=new int[longitud];
+			
+			//Creamos un bucle For para rellenar de numeros enteros aleatorios
+			//del 1 al 10 con la funcion numeroAleatorio el array
+			for(int i=0;i<=array.length;i++) {
+				array[i]=numeroAleatorio(1,10);
+				System.out.println("En la posición "+i+" del array se encuentra el dato: "+array[i]);
+			}
+			
+			System.out.println("El contenido completo del array es: "+array);
+			
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	//Función numero aleatorio entre dos numeros enteros que le demos
+    public static int numeroAleatorio(int minimo, int maximo){
+        return (int)Math.floor(Math.random()*(minimo-(maximo+1))+(maximo+1));
+    }
+    
+    
+
+}

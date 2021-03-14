1f7f876c22f516fd28dbf36058793386566d0b38
Javier Baena Santa-Cruz <73710320+jb-coder@users.noreply.github.com>
2021-03-14 12:37:37 +0100

    Update Tres.java

--- UD5/Tres.java
+++ UD5/Tres.java
@@ -2,24 +2,27 @@
 4. En el archivo tres.java, realice un programa que lea de un archivo datos.txt
 los datos de un directorio telefónico y los muestre por pantalla.
 */
-import java.io.IOException;
-import java.io.FileReader;
+import java.io.*;
 
 public class Tres {
 	public static void main(String[] args) throws IOException {
-		
 		try {
 			
-			FileReader fr = new FileReader ("C:\\EjerciciosStreams\\datos.txt");
+			//Objeto para leer el archivo datos.txt codificado en UTF-8
+			BufferedReader bf = new BufferedReader(new InputStreamReader (new FileInputStream ("C:\\EjerciciosStreams\\datos.txt"),"UTF-8"));
+			
+			//String donde se almacenara el contenido de datos.txt
 			String contenido ="";
+			//Variable para recorrer cada caracter de datos.txt
 			int c;
 			
-			while ((c = fr.read())!=-1) {
+			//Bucle que ira recorriendo cada caracter de datos.txt, -1 es el ultimo byte del archivo
+			while ((c = bf.read())!=-1) {
 				contenido+=(char)c;
 			}
 			System.out.println(contenido);
 			
-			fr.close();
+			bf.close();
 		
 		}catch (IOException e) {
 			e.printStackTrace();

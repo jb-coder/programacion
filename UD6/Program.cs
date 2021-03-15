f2a40e661c680e7e976c8d3ffd8df18cad091cf6
Javier Baena Santa-Cruz <73710320+jb-coder@users.noreply.github.com>
2021-03-15 18:30:08 +0100

    Add files via upload

---
+++ UD6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cchar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <int> nums=new Queue<int>();
+
+            //Agregar elementos a la cola
+
+            foreach (int nums in new int[6] {6, 3, 5, 8, 5, 1}){
+                nums.Enqueue(nums);
+            }
+
+            //Recorriendo la cola
+
+            Console.WriteLine("Recorriendo la cola");
+
+            foreach (int nums in nums){
+                Console.WriteLine(nums);
+            }
+
+            //Eliminar el primer elemento de la cola
+            Console.WriteLine("Eliminando elementos");
+
+            foreach(int nums in nums){
+                Console.WriteLine(nums);
+            }
+        }
+    }
+}
